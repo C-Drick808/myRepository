3e3daed9e507cf4881cd1c9335ad48136b28e5f0
C-Drick808 <cedrickesd@gmail.com>
2023-12-22 16:06:03 +0800

    fixing simple error case sensitive removing App on class name HelloWorldApp and adding double quotes to the print line to display the string

---
+++ java_tesda/HelloWorld.java
@@ -0,0 +1,12 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+package java_tesda;
+class HelloWorld {
+
+   public static void main(String[] args){
+      System.out.println("Kamusta Mundo!");// Display the string.
+   }
+    
+}
