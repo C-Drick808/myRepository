8351142bcc7c4b5e0d62e58dce488ec9e9f15d1e
C-Drick808 <cedrickesd@gmail.com>
2023-12-22 16:56:42 +0800

    Change that target method of DateTask use it the other one that have parameter to pass it to the super class. and setting up the month.

--- java_tesda/Task121/MyDate.java
+++ java_tesda/Task121/MyDate.java
@@ -2,7 +2,7 @@ package Task121;
 public class MyDate {
     public static void main(String[] args) {
         DateTask date1 = new DateTask();
-        DateTask date2 = new DateTask();
+        DateTask date2 = new DateTask(12, 28, 2004);
         System.out.println(date1.toString());
         System.out.println(date2.toString());
        // display leapyears
