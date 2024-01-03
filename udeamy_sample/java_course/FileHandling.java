c0e15d064af0dc75a69d5bf0a0fdc1c9005f78da
C-Drick808 <cedrickesd@gmail.com>
2024-01-03 14:43:24 +0800

    clean code

--- udeamy_sample/java_course/FileHandling.java
+++ udeamy_sample/java_course/FileHandling.java
@@ -5,6 +5,7 @@ public class FileHandling {
 
    public static void main(String[] args) throws IOException {
 
+      
       FileInputStream fileIn = null;
       FileOutputStream fileOut = null;
       try{
